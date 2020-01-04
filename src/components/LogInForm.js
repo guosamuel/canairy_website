7d39fc3f07ffa4793fa0a045c1ebf472dfe894d2
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-01-04 15:24:57 -0500

    componentized the form

---
+++ src/components/LogInForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LogInForm() {
+  return (
+    <div>
+      <h1>Derp</h1>
+    </div>
+  )
+}
+
+export default LogInForm
--- src/containers/LogIn.js
+++ src/containers/LogIn.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import LogInForm from '../components/LogInForm'
 
 function LogIn() {
   return (
     <div>
       <h2>Welcome to Canairy! Please log in to view your profile</h2>
+      <LogInForm />
     </div>
   )
 }

818d13a2a9e91cdeb98927507d7a379169a3ae7e
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-01-04 15:36:03 -0500

    made the log in form a controller component

--- src/components/LogInForm.js
+++ src/components/LogInForm.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function LogInForm() {
+  const [ username, setUsername ] = useState("")
+  const [ password, setPassword ] = useState("")
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const handlePassword = e => {
+    setPassword(e.target.value)
+  }
+
   return (
     <div>
-      <h1>Derp</h1>
+      <form>
+        <label>Username</label>
+        <br />
+        <input
+          value={username}
+          onChange={handleUsername}
+        />
+        <br />
+        <label>Password</label>
+        <br />
+        <input
+          value={password}
+          onChange={handlePassword}
+          />
+        <br />
+        <button>Log In</button>
+      </form>
     </div>
   )
 }

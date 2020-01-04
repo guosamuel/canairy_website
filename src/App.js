1d7c0bc2dc866d1bf2f8a9a051aec30e87de6ac1
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-01-04 15:22:55 -0500

    made a LogIn container to contain a message and the log in form itself

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header'
+import LogIn from './containers/LogIn'
 
 function App() {
   return (
     <div>
       <Header />
+      <LogIn />
     </div>
   );
 }
---
+++ src/containers/LogIn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LogIn() {
+  return (
+    <div>
+      <h2>Welcome to Canairy! Please log in to view your profile</h2>
+    </div>
+  )
+}
+
+export default LogIn

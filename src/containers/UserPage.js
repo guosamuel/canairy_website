ce5016d666b090e80c2b918c035ea7c3e21ea316
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-01-07 15:05:54 -0500

    downloaded a spinner to represent loading

--- src/containers/UserPage.js
+++ src/containers/UserPage.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import UserPageHeading from '../components/UserPageHeading'
 import LogOutButton from '../components/LogOutButton'
+import PacmanLoader from 'react-spinners/PacmanLoader'
 
 function UserPage(props){
   const [ loading, setLoading ] = useState(true)
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 1000)
+    setTimeout(() => setLoading(false), 2000)
     // NEED TO TALK WITH DANIEL HOW THE ENDPOINT WOULD WORK AND HOW THE TOKEN IS GONNA BE GENERATED
     // NEED TO SET THE LOADING TO FALSE IN THE .then()
     // fetch("dev.canairy.ai/user")
@@ -17,7 +18,7 @@ function UserPage(props){
   return (
     <div>
       {loading ?
-        <h1>Still Loading...</h1> :
+        <PacmanLoader loading={loading} /> :
         <React.Fragment>
           <UserPageHeading />
           <LogOutButton />

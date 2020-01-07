9bb1429b6165850188b77612432f66051298ea6b
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-01-07 14:57:04 -0500

    able to imitate a loading page

--- src/containers/UserPage.js
+++ src/containers/UserPage.js
@@ -1,17 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import UserPageHeading from '../components/UserPageHeading'
 import LogOutButton from '../components/LogOutButton'
 
 function UserPage(props){
+  const [ loading, setLoading ] = useState(true)
 
   useEffect(() => {
-    console.log("derp")
+    setTimeout(() => setLoading(false), 1000)
+    // NEED TO TALK WITH DANIEL HOW THE ENDPOINT WOULD WORK AND HOW THE TOKEN IS GONNA BE GENERATED
+    // NEED TO SET THE LOADING TO FALSE IN THE .then()
+    // fetch("dev.canairy.ai/user")
+    //   .then(resp => resp.json())
+    //   .then()
   }, [])
 
   return (
     <div>
-      <UserPageHeading />
-      <LogOutButton />
+      {loading ?
+        <h1>Still Loading...</h1> :
+        <React.Fragment>
+          <UserPageHeading />
+          <LogOutButton />
+        </React.Fragment>
+      }
     </div>
   )
 }

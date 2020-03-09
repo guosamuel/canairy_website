e6aa0202f919920222371e15bea09dbd259d5222
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-03-09 16:32:50 -0400

    reorganized the dashboard to match the wire frame more closely

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -57,45 +57,7 @@ class Dashboard extends Component {
       <div className="content">
         <Grid fluid>
           <Row>
-            <Col lg={3} sm={6}>
-              <StatsCard
-                bigIcon={<i className="pe-7s-server text-warning" />}
-                statsText="Capacity"
-                statsValue="105GB"
-                statsIcon={<i className="fa fa-refresh" />}
-                statsIconText="Updated now"
-              />
-            </Col>
-            <Col lg={3} sm={6}>
-              <StatsCard
-                bigIcon={<i className="pe-7s-wallet text-success" />}
-                statsText="Revenue"
-                statsValue="$1,345"
-                statsIcon={<i className="fa fa-calendar-o" />}
-                statsIconText="Last day"
-              />
-            </Col>
-            <Col lg={3} sm={6}>
-              <StatsCard
-                bigIcon={<i className="pe-7s-graph1 text-danger" />}
-                statsText="Errors"
-                statsValue="23"
-                statsIcon={<i className="fa fa-clock-o" />}
-                statsIconText="In the last hour"
-              />
-            </Col>
-            <Col lg={3} sm={6}>
-              <StatsCard
-                bigIcon={<i className="fa fa-twitter text-info" />}
-                statsText="Followers"
-                statsValue="+45"
-                statsIcon={<i className="fa fa-refresh" />}
-                statsIconText="Updated now"
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col md={8}>
+            <Col>
               <Card
                 statsIcon="fa fa-history"
                 id="chartHours"
@@ -117,7 +79,9 @@ class Dashboard extends Component {
                 }
               />
             </Col>
-            <Col md={4}>
+          </Row>
+          <Row>
+            <Col md={6}>
               <Card
                 statsIcon="fa fa-clock-o"
                 title="Coughs Detected"
@@ -135,44 +99,21 @@ class Dashboard extends Component {
                 }
               />
             </Col>
-          </Row>
-
-          <Row>
             <Col md={6}>
               <Card
-                id="chartActivity"
-                title="2014 Sales"
-                category="All products including Taxes"
-                stats="Data information certified"
-                statsIcon="fa fa-check"
+                statsIcon="fa fa-clock-o"
+                title="Coughs Detected"
+                stats="Updated now"
                 content={
-                  <div className="ct-chart">
-                    <ChartistGraph
-                      data={dataBar}
-                      type="Bar"
-                      options={optionsBar}
-                      responsiveOptions={responsiveBar}
-                    />
+                  <div
+                    id="chartPreferences"
+                    className="ct-chart ct-perfect-fourth"
+                  >
+                    <ChartistGraph data={coughsDetectedDataPie} options={optionsCoughsDetected} type="Pie" />
                   </div>
                 }
                 legend={
-                  <div className="legend">{this.createLegend(legendBar)}</div>
-                }
-              />
-            </Col>
-
-            <Col md={6}>
-              <Card
-                title="Tasks"
-                category="Backend development"
-                stats="Updated 3 minutes ago"
-                statsIcon="fa fa-history"
-                content={
-                  <div className="table-full-width">
-                    <table className="table">
-                      <Tasks />
-                    </table>
-                  </div>
+                  <div className="legend">{this.createLegend(coughsDetectedLegendPie)}</div>
                 }
               />
             </Col>
